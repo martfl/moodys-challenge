bbe2fc00a98795783e039eccb55415715b4b2af1
Martín Fernández Lorenzo <martin@Martins-MacBook-Pro.local>
2023-05-09 03:28:54 -0600

    write docstrings

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ import sys
 from typing import List, Tuple
 
 class Rover:
+    """
+    Represents a Mars rover with a position (x, y) and a direction (N, E, S, W).
+    The rover can process instructions to move or turn on a plateau of given size.
+    """
+
     DIRECTIONS = {
         'N': (0, 1),
         'E': (1, 0),
@@ -28,15 +33,28 @@ class Rover:
         self.max_x, self.max_y = plateau_size
 
     def process_instruction(self, instruction: str) -> None:
+        """
+        Process a single instruction to either move the rover or change its direction.
+
+        :param instruction: A single-character string representing the instruction ('L', 'R', or 'M').
+        """
         if instruction in ('L', 'R'):
             self._turn(instruction)
         elif instruction == 'M':
             self._move()
 
     def _turn(self, direction: str) -> None:
+        """
+        Update the rover's direction based on the given turn instruction ('L' or 'R').
+
+        :param direction: A single-character string representing the turn instruction ('L' for left or 'R' for right).
+        """
         self.direction = self.TURNS[(self.direction, direction)]
 
     def _move(self) -> None:
+        """
+        Move the rover one grid point forward in its current direction, if the new position is within the plateau boundaries.
+        """
         dx, dy = self.DIRECTIONS[self.direction]
         new_x = self.x + dx
         new_y = self.y + dy
@@ -49,6 +67,14 @@ class Rover:
 
 # process final rover position from instruction list 
 def process_rover(pos: str, instructions: str, plateau_size: Tuple[int, int]) -> str:
+    """
+    Process rover's position and instructions to determine its final position on the plateau.
+
+    :param pos: A string representing the rover's initial position (e.g., '1 2 N').
+    :param instructions: A string containing space-separated instructions for the rover (e.g., 'L M L M L M L M M').
+    :param plateau_size: A tuple containing the plateau size (max_x, max_y).
+    :return: A string representing the rover's final position (e.g., '1 3 N').
+    """
     x, y, d = pos.split()
     rover = Rover(int(x), int(y), d, plateau_size)
     for instruction in instructions:
@@ -56,6 +82,12 @@ def process_rover(pos: str, instructions: str, plateau_size: Tuple[int, int]) ->
     return str(rover)
 
 def parse_input(input_str: str) -> Tuple[Tuple[int, int], List[Tuple[str, str]]]:
+    """
+    Parse the input string and extract plateau size and rover information.
+
+    :param input_str: A string containing the formatted input data to validate
+    :return: A tuple containing the plateau size as a tuple of integers (max_x, max_y) and a list of rover information.
+    """
     input_list = input_str.strip().split("\n")
 
     # validate and extract plateau size
@@ -82,14 +114,13 @@ def parse_input(input_str: str) -> Tuple[Tuple[int, int], List[Tuple[str, str]]]
 
     return plateau_size, rover_data
 
-# main function
 def main():
+    """
+    Main function to read input data, process rover instructions, and output the final positions of all rovers.
+    """
     # input format: upper-right coordinates of the plateau, followed by rover positions and instructions
-    input_str = sys.stdin.read().strip()
-    input_list = input_str.split("\n")
-    
     try:
-        plateau_size, rover_data = parse_input(input_str)
+        plateau_size, rover_data = parse_input(sys.stdin.read())
     except ValueError as e:
         print(e)
         return
